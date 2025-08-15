7ed97c440d4afd945e6d935e59090943338fc80e
Shirley Oduor <shirleycaren254@gmail.com>
2025-08-15 16:13:36 -0700

    new file and app.js modified

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,7 @@
+// src/react-app-env.d.ts
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
