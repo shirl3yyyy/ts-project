cf48544b41ca66c61ec91e142cd740acaa20babd
Shirley Oduor <shirleycaren254@gmail.com>
2025-08-15 17:29:28 -0700

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Welcome to the Counter App</h1>
+      <Counter />
     </div>
   );
 }
